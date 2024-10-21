f80b90a6dbeec587be8ee6c17ba28e83d1a8f9da
LanaTherrien03 <la576625@ucf.edu>
2024-10-21 17:12:50 -0400

    Added the weather script
    Still a work in progress.

--- Assets/WeatherStuff/LightiningScript.cs
+++ Assets/WeatherStuff/LightiningScript.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class LightiningScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject lightningOne;
+    public GameObject lightningTwo;
+    public GameObject lightningThree;
+    public GameObject audioOne;
+
+
+    private void start()
     {
-        
+        lightningOne.SetActive(false);
+        lightningTwo.SetActive(false);
+        lightningThree.SetActive(false);
+
+        audioOne.SetActive(false);
+
+        Invoke("CallLightning", 1.75f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void CallLightning()
     {
-        
+        int r = Random.Range(0, 3);
+        if(r==0)
+        {
+            lightningOne.SetActive(true);
+            Invoke("EndLightning", .125f);
+            Invoke("CallThunder", .395f);
+        }
+        else if (r==1)
+        {
+            lightningTwo.SetActive(true);
+            Invoke("EndLightning", .105f);
+            Invoke("CallThunder", .195f);
+        }
+        else
+        {
+            lightningThree.SetActive(true);
+            Invoke("EndLightning", .75f);
+            CallThunder();
+        }
+ 
+    }
+
+    void EndLightning()
+    {
+        lightningOne.SetActive(false);
+        lightningTwo.SetActive(false);
+        lightningThree.SetActive(false);
+
+        float rand = Random.Range(30f, 40f);
+        Invoke("CallLightning", rand);
+    }
+
+    void CallThunder()
+    {
+        audioOne.SetActive(true);
+        Invoke("EndThunder", 29f);
+    }
+
+    void EndThunder()
+    {
+        audioOne.SetActive(false);
     }
 }
